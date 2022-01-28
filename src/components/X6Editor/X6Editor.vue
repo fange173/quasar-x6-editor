3e3c0ea0e92d279a1364d7bbe4672ea2c0112506
Fange <928457421@qq.com>
2022-01-28 23:33:06 +0800

    change 规范化工作流数据结构，规范化导入和导出方法

--- src/components/X6Editor/X6Editor.vue
+++ src/components/X6Editor/X6Editor.vue
@@ -19,6 +19,9 @@
 
         <q-list dense style="min-width: 100px" v-if="choiceType == 'edge'">
           <q-item clickable v-close-popup>
+            <q-item-section @click="showEdge">查看</q-item-section>
+          </q-item>
+          <q-item clickable v-close-popup>
             <q-item-section @click="deleteEdge">删除</q-item-section>
           </q-item>
         </q-list>
@@ -102,7 +105,7 @@ export default defineComponent({
     // 是否显示Dialog
     const openHelpDialog = ref(false);
     const openCodeDialog = ref(false);
-    const jsonData = ref('');
+    const jsonData = ref({});
     // 是否显示网点
     const showGird = ref(true);
     // 连接桩
@@ -142,83 +145,54 @@ export default defineComponent({
         },
       ],
     };
-
-    // 初始化节点数据
-    let data = {
-      // 节点
-      nodes: [
+    // 原始json数据
+    const json = {
+      name: '测试工作流20220128-1',
+      externalId: 'w-workflow-20220128',
+      materialName: 'w',
+      jobs: [
         {
-          id: 'fe94ef60-b536-22bb-61fd-6b3a2222be17', // String，可选，节点的唯一标识
-          shape: 'default-node',
-          data: {
-            name: 'Simulate1脚本',
-            cpuCores: '2',
-            appUseCase: 'ab08ad37-055b-b368-58aa-037620228cf8',
-            status: 'default',
-          },
-          ports: { ...ports },
+          externalId: 'fe94ef60-b536-22bb-61fd-6b3a2222be17',
+          cpuCores: '2',
+          appUseCase: 'ab08ad37-055b-b368-58aa-037620228cf8',
+          name: 'Simulate1脚本',
         },
         {
-          id: '4557647d-19bc-b1d7-5aec-4963dfe83b28',
-          shape: 'default-node',
-          data: {
-            name: 'Simulate2脚本',
-            cpuCores: '1',
-            appUseCase: 'f30223f0-a215-4402-38ba-067a917b095d',
-            status: 'default',
-          },
-          ports: { ...ports },
+          externalId: '4557647d-19bc-b1d7-5aec-4963dfe83b28',
+          cpuCores: '1',
+          appUseCase: 'f30223f0-a215-4402-38ba-067a917b095d',
+          name: 'Simulate2脚本',
         },
         {
-          id: '35344a12-c8d7-1e2c-704f-b10a9b56e2c6',
-          shape: 'default-node',
-          data: {
-            name: 'Simulate3脚本',
-            cpuCores: '2',
-            appUseCase: 'c0121580-1097-418f-14be-170e474fb8d3',
-            status: 'default',
-          },
-          ports: { ...ports },
+          externalId: '35344a12-c8d7-1e2c-704f-b10a9b56e2c6',
+          cpuCores: '2',
+          appUseCase: 'c0121580-1097-418f-14be-170e474fb8d3',
+          name: 'Simulate3脚本',
         },
         {
-          id: '8c6f878b-ffa0-e51b-93d2-0e65344d7f56',
-          shape: 'default-node',
-          data: {
-            name: 'Simulate4脚本',
-            cpuCores: '2',
-            appUseCase: 'f4f1357b-1bfc-6797-5030-b9822d910e73',
-            status: 'default',
-          },
-          ports: { ...ports },
+          externalId: '8c6f878b-ffa0-e51b-93d2-0e65344d7f56',
+          cpuCores: '2',
+          appUseCase: 'f4f1357b-1bfc-6797-5030-b9822d910e73',
+          name: 'Simulate4脚本',
         },
         {
-          id: 'ba2f49b7-912e-de79-50a3-8e6d4083f681',
-          shape: 'default-node',
-          data: {
-            name: 'Simulate5脚本',
-            cpuCores: '1',
-            appUseCase: '1d6913e0-32bd-4563-a073-4900579da937',
-            status: 'default',
-          },
-          ports: { ...ports },
+          externalId: 'ba2f49b7-912e-de79-50a3-8e6d4083f681',
+          cpuCores: '1',
+          appUseCase: '1d6913e0-32bd-4563-a073-4900579da937',
+          name: 'Simulate5脚本',
         },
         {
-          id: '7a65b4d8-12bd-bd09-170c-cf37cc0a568a',
-          shape: 'default-node',
-          data: {
-            name: 'Simulate6脚本',
-            cpuCores: '2',
-            appUseCase: '81abf716-a0f8-f064-cabe-6e4e44442825',
-            status: 'default',
-          },
-          ports: { ...ports },
+          externalId: '7a65b4d8-12bd-bd09-170c-cf37cc0a568a',
+          cpuCores: '2',
+          appUseCase: '81abf716-a0f8-f064-cabe-6e4e44442825',
+          name: 'Simulate6脚本',
         },
       ],
-      // 边
-      edges: [
+      dependencies: [
         {
-          source: 'fe94ef60-b536-22bb-61fd-6b3a2222be17', // String，必须，起始节点 id
-          target: '4557647d-19bc-b1d7-5aec-4963dfe83b28', // String，必须，目标节点 id
+          externalId: 'ba832c2b-7ff3-4b24-be99-53ad0f949161',
+          jobExternalId: '4557647d-19bc-b1d7-5aec-4963dfe83b28',
+          jobDependencyExternalId: 'fe94ef60-b536-22bb-61fd-6b3a2222be17',
           fileDependencies: [
             {
               targetAppFile: '0a9c2cb6-3dc7-9a00-55f1-33f2e31f5561',
@@ -231,12 +205,11 @@ export default defineComponent({
               dependencyType: 'appFile',
             },
           ],
-          shape: 'default-edge',
-          zIndex: -1,
         },
         {
-          source: 'fe94ef60-b536-22bb-61fd-6b3a2222be17',
-          target: '35344a12-c8d7-1e2c-704f-b10a9b56e2c6',
+          externalId: '6201ebaf-5c4a-4211-a9ca-7ecb367c9e25',
+          jobExternalId: '35344a12-c8d7-1e2c-704f-b10a9b56e2c6',
+          jobDependencyExternalId: 'fe94ef60-b536-22bb-61fd-6b3a2222be17',
           fileDependencies: [
             {
               targetAppFile: 'b098bf40-eb8c-30d8-ef16-8f21ab41b248',
@@ -244,12 +217,11 @@ export default defineComponent({
               dependencyType: 'appFile',
             },
           ],
-          shape: 'default-edge',
-          zIndex: -1,
         },
         {
-          source: '4557647d-19bc-b1d7-5aec-4963dfe83b28',
-          target: '8c6f878b-ffa0-e51b-93d2-0e65344d7f56',
+          externalId: 'a5b8558d-12c8-4ec8-a69f-cbcc7e1dbea7',
+          jobExternalId: '8c6f878b-ffa0-e51b-93d2-0e65344d7f56',
+          jobDependencyExternalId: '4557647d-19bc-b1d7-5aec-4963dfe83b28',
           fileDependencies: [
             {
               targetAppFile: '56c90586-a686-5330-0b16-72a231212171',
@@ -257,23 +229,24 @@ export default defineComponent({
               dependencyType: 'appFile',
             },
           ],
-          shape: 'default-edge',
-          zIndex: -1,
         },
         {
-          source: '4557647d-19bc-b1d7-5aec-4963dfe83b28',
-          target: 'ba2f49b7-912e-de79-50a3-8e6d4083f681',
-          shape: 'default-edge',
-          zIndex: -1,
+          externalId: '9eda6ea8-d91b-4b1c-9cfb-4a46be11b698',
+          jobExternalId: 'ba2f49b7-912e-de79-50a3-8e6d4083f681',
+          jobDependencyExternalId: '4557647d-19bc-b1d7-5aec-4963dfe83b28',
         },
         {
-          source: '35344a12-c8d7-1e2c-704f-b10a9b56e2c6',
-          target: '7a65b4d8-12bd-bd09-170c-cf37cc0a568a',
-          shape: 'default-edge',
-          zIndex: -1,
+          externalId: '080cf216-4e96-4b31-b664-8666e3f98b41',
+          jobExternalId: '7a65b4d8-12bd-bd09-170c-cf37cc0a568a',
+          jobDependencyExternalId: '35344a12-c8d7-1e2c-704f-b10a9b56e2c6',
         },
       ],
     };
+    // 初始化节点数据
+    const flowName = ref('');
+    const flowId = ref('');
+    const flowMaterialName = ref('');
+    let data: any = { nodes: [], edges: [] };
 
     // 撤销
     const undo = () => {
@@ -341,18 +314,62 @@ export default defineComponent({
     };
     // 查看节点数据
     const showNode = () => {
+      transformToJson();
       jsonData.value = JSON.stringify(
-        data.nodes.find(ele => {
-          return ele.id == choiceId.value;
+        (jsonData.value as any).jobs.find((ele: { externalId: string }) => {
+          return ele.externalId == choiceId.value;
         }),
         null,
         2
       );
       openCodeDialog.value = true;
     };
+    // 查看连线数据
+    const showEdge = () => {
+      transformToJson();
+      jsonData.value = JSON.stringify(
+        (jsonData.value as any).dependencies.find((ele: { externalId: string }) => {
+          return ele.externalId == choiceId.value;
+        }),
+        null,
+        2
+      );
+      openCodeDialog.value = true;
+    };
+    // x6数据转json
+    const transformToJson = () => {
+      const data = graph.toJSON().cells;
+      let jobs: any = [];
+      let dependencies: any = [];
+      for (let i = 0, len = data.length; i < len; i++) {
+        if (data[i].shape === 'default-edge') {
+          dependencies.push({
+            externalId: data[i].id,
+            jobExternalId: data[i].target.cell,
+            jobDependencyExternalId: data[i].source.cell,
+            fileDependencies: data[i].fileDependencies,
+          });
+        } else {
+          jobs.push({
+            externalId: data[i].id,
+            cpuCores: data[i].data.cpuCores,
+            appUseCase: data[i].data.appUseCase,
+            name: data[i].data.name,
+          });
+        }
+      }
+      jsonData.value = {
+        name: '测试工作流20220128-1',
+        externalId: 'w-workflow-20220128',
+        materialName: 'w',
+        jobs: jobs,
+        dependencies: dependencies,
+      };
+    };
     // 查看工作流数据
     const showData = () => {
-      jsonData.value = JSON.stringify(graph.toJSON(), null, 2);
+      transformToJson();
+      jsonData.value = JSON.stringify(jsonData.value, null, 2);
       openCodeDialog.value = true;
     };
     const closeCode = () => {
@@ -421,7 +438,40 @@ export default defineComponent({
       // eslint-disable-next-line
       (dnd.value as any).start(node, e);
     };
-
+    const transformToData = () => {
+      if (json.name) flowName.value = json.name;
+      if (json.externalId) flowId.value = json.externalId;
+      if (json.materialName) flowMaterialName.value = json.materialName;
+      if (json.jobs.length !== 0) {
+        const jobs = json.jobs;
+        for (let i = 0, len = jobs.length; i < len; i++) {
+          data.nodes.push({
+            id: jobs[i].externalId,
+            data: {
+              name: jobs[i].name,
+              cpuCores: jobs[i].cpuCores,
+              appUseCase: jobs[i].appUseCase,
+              status: 'default',
+            },
+            shape: 'default-node',
+            ports: { ...ports },
+          });
+        }
+      }
+      if (json.dependencies.length !== 0) {
+        const dependencies = json.dependencies;
+        for (let i = 0, len = dependencies.length; i < len; i++) {
+          data.edges.push({
+            id: dependencies[i].externalId,
+            source: dependencies[i].jobDependencyExternalId,
+            target: dependencies[i].jobExternalId,
+            fileDependencies: dependencies[i].fileDependencies,
+            shape: 'default-edge',
+            zIndex: -1,
+          });
+        }
+      }
+    };
     // 初始化自定义节点、连线形状
     const initShape = () => {
       Graph.registerNode(
@@ -680,6 +730,7 @@ export default defineComponent({
     };
 
     onMounted(() => {
+      transformToData();
       initShape();
       initGraph();
       initDnd();
@@ -699,6 +750,7 @@ export default defineComponent({
       refresh,
       nodeSetting,
       showNode,
+      showEdge,
       showData,
       downloadData,
       shGird,
