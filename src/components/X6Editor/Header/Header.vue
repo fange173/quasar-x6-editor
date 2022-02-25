1f4e0eb06ed9d9cf05bb5af9b90855a31d4476c6
Fange <928457421@qq.com>
2022-02-25 16:41:29 +0800

    change: 组件化Header

---
+++ src/components/X6Editor/Header/Header.vue
@@ -0,0 +1,232 @@
+<template>
+  <div>
+    <q-header class="my-header">
+      <q-toolbar class="text-black">
+        <q-avatar>
+          <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-black.svg" />
+        </q-avatar>
+        <q-toolbar-title>
+          <div class="text-body1 text-bold q-px-sm">{{ flow.name }}</div>
+          <div class="text-body2 row items-center">
+            <div class="q-px-sm cursor-pointer non-selectable relative-position flex flex-center toolbar-a" v-ripple>
+              文件
+              <q-menu>
+                <q-list dense style="min-width: 100px;">
+                  <q-item clickable v-close-popup @click="showData">
+                    <q-item-section>
+                      查看数据
+                    </q-item-section>
+                  </q-item>
+                  <q-item clickable v-close-popup @click="downloadData">
+                    <q-item-section>
+                      导出数据
+                    </q-item-section>
+                  </q-item>
+                  <q-separator />
+                  <q-item clickable v-close-popup @click="showData">
+                    <q-item-section>
+                      保存！
+                    </q-item-section>
+                  </q-item>
+                  <q-separator />
+                  <q-item clickable v-close-popup @click="showData">
+                    <q-item-section>
+                      关闭！
+                    </q-item-section>
+                  </q-item>
+                </q-list>
+              </q-menu>
+            </div>
+            <div class="q-px-sm cursor-pointer non-selectable relative-position flex flex-center toolbar-a" v-ripple>
+              编辑
+              <q-menu>
+                <q-list dense style="min-width: 100px;">
+                  <q-item clickable v-close-popup @click="undo">
+                    <q-item-section>
+                      撤销
+                    </q-item-section>
+                  </q-item>
+                  <q-item clickable v-close-popup @click="redo">
+                    <q-item-section>
+                      重做
+                    </q-item-section>
+                  </q-item>
+                  <q-separator />
+                  <q-item clickable>
+                    <q-item-section>更多</q-item-section>
+                    <q-item-section side>
+                      <q-icon name="keyboard_arrow_right" />
+                    </q-item-section>
+                    <q-menu anchor="top end" self="top start">
+                      <q-list dense style="min-width: 100px">
+                        <q-item clickable>
+                          <q-item-section @click="refresh">清空</q-item-section>
+                        </q-item>
+                      </q-list>
+                    </q-menu>
+                  </q-item>
+                </q-list>
+              </q-menu>
+            </div>
+            <div class="q-px-sm cursor-pointer non-selectable relative-position flex flex-center toolbar-a" v-ripple>
+              视图
+              <q-menu>
+                <q-list dense style="min-width: 100px;">
+                  <q-item clickable @click="shMinimap">
+                    <q-item-section>
+                      显示缩略图
+                    </q-item-section>
+                    <q-item-section side>
+                      <q-icon left name="check_box_outline_blank" class="q-mr-none" v-if="!showMinimap" />
+                      <q-icon left name="check_box" class="q-mr-none" v-else />
+                    </q-item-section>
+                  </q-item>
+                  <q-item clickable @click="shGrid">
+                    <q-item-section>
+                      显示网点
+                    </q-item-section>
+                    <q-item-section side>
+                      <q-icon left name="check_box_outline_blank" class="q-mr-none" v-if="!showGrid" />
+                      <q-icon left name="check_box" class="q-mr-none" v-else />
+                    </q-item-section>
+                  </q-item>
+                  <q-separator />
+                  <q-item clickable @click="zoomIn">
+                    <q-item-section>
+                      放大
+                    </q-item-section>
+                    <q-item-section side>
+                      Ctrl+滚轮向上
+                    </q-item-section>
+                  </q-item>
+                  <q-item clickable @click="zoomOut">
+                    <q-item-section>
+                      缩小
+                    </q-item-section>
+                    <q-item-section side>
+                      Ctrl+滚轮向下
+                    </q-item-section>
+                  </q-item>
+                  <q-separator />
+                  <q-item clickable @click="centerContent">
+                    <q-item-section>
+                      居中
+                    </q-item-section>
+                  </q-item>
+                  <q-item clickable @click="layout">
+                    <q-item-section>
+                      自动布局
+                    </q-item-section>
+                  </q-item>
+                  <q-separator />
+                  <q-item clickable @click="zoomTo(1)">
+                    <q-item-section>
+                      重置比例
+                    </q-item-section>
+                  </q-item>
+                  <q-item clickable @click="zoomToFit">
+                    <q-item-section>
+                      适应内容
+                    </q-item-section>
+                  </q-item>
+                </q-list>
+              </q-menu>
+            </div>
+            <div class="q-px-sm cursor-pointer non-selectable relative-position flex flex-center toolbar-a" v-ripple @click="help">
+              帮助
+            </div>
+          </div>
+        </q-toolbar-title>
+        <q-btn dense push color="primary" icon="autorenew" label="运行" @click="runWorkFlow" />
+      </q-toolbar>
+    </q-header>
+  </div>
+</template>
+
+<script>
+import { getCurrentInstance } from 'vue';
+
+export default {
+  props: {
+    showGrid: {
+      type: Boolean,
+    },
+    showMinimap: {
+      type: Boolean,
+    },
+    flow: {
+      type: Object,
+    },
+  },
+  setup() {
+    const _this = getCurrentInstance();
+
+    const undo = () => {
+      _this.parent.proxy.undo();
+    };
+    const redo = () => {
+      _this.parent.proxy.redo();
+    };
+    const zoomIn = () => {
+      _this.parent.proxy.zoomIn();
+    };
+    const zoomOut = () => {
+      _this.parent.proxy.zoomOut();
+    };
+    const centerContent = () => {
+      _this.parent.proxy.centerContent();
+    };
+    const shGrid = () => {
+      _this.parent.proxy.shGrid();
+    };
+    const shMinimap = () => {
+      _this.parent.proxy.shMinimap();
+    };
+    const layout = () => {
+      _this.parent.proxy.layout();
+    };
+    const showData = () => {
+      _this.parent.proxy.showData();
+    };
+    const downloadData = () => {
+      _this.parent.proxy.downloadData();
+    };
+    const runWorkFlow = () => {
+      _this.parent.proxy.runWorkFlow();
+    };
+    const help = () => {
+      _this.parent.proxy.help();
+    };
+    const zoomTo = (num) => {
+      _this.parent.proxy.zoomTo(num);
+    };
+    const zoomToFit = () => {
+      _this.parent.proxy.zoomToFit();
+    };
+
+    return {
+      undo,
+      redo,
+      zoomIn,
+      zoomOut,
+      centerContent,
+      shGrid,
+      layout,
+      showData,
+      downloadData,
+      runWorkFlow,
+      help,
+      shMinimap,
+      zoomTo,
+      zoomToFit,
+    };
+  },
+};
+</script>
+
+<style scoped>
+@import url('../X6Editor.css');
+.q-item__section--side > .q-icon {
+  margin-right: -8px;
+}
+</style>
--- src/components/X6Editor/X6Editor.vue
+++ src/components/X6Editor/X6Editor.vue
@@ -1,145 +1,6 @@
 <template>
   <div>
-    <q-header class="my-header">
-      <q-toolbar class="text-black">
-        <q-avatar>
-          <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-black.svg" />
-        </q-avatar>
-        <q-toolbar-title>
-          <div class="text-body1 text-bold q-px-sm">{{ flow.name }}</div>
-          <div class="text-body2 row items-center">
-            <div class="q-px-sm cursor-pointer non-selectable relative-position flex flex-center toolbar-a" v-ripple>
-              文件
-              <q-menu>
-                <q-list dense style="min-width: 100px;">
-                  <q-item clickable v-close-popup @click="showData">
-                    <q-item-section>
-                      查看数据
-                    </q-item-section>
-                  </q-item>
-                  <q-item clickable v-close-popup @click="downloadData">
-                    <q-item-section>
-                      导出数据
-                    </q-item-section>
-                  </q-item>
-                  <q-separator />
-                  <q-item clickable v-close-popup @click="showData">
-                    <q-item-section>
-                      保存！
-                    </q-item-section>
-                  </q-item>
-                  <q-separator />
-                  <q-item clickable v-close-popup @click="showData">
-                    <q-item-section>
-                      关闭！
-                    </q-item-section>
-                  </q-item>
-                </q-list>
-              </q-menu>
-            </div>
-            <div class="q-px-sm cursor-pointer non-selectable relative-position flex flex-center toolbar-a" v-ripple>
-              编辑
-              <q-menu>
-                <q-list dense style="min-width: 100px;">
-                  <q-item clickable v-close-popup @click="undo">
-                    <q-item-section>
-                      撤销
-                    </q-item-section>
-                  </q-item>
-                  <q-item clickable v-close-popup @click="redo">
-                    <q-item-section>
-                      重做
-                    </q-item-section>
-                  </q-item>
-                  <q-separator />
-                  <q-item clickable>
-                    <q-item-section>更多</q-item-section>
-                    <q-item-section side>
-                      <q-icon name="keyboard_arrow_right" />
-                    </q-item-section>
-                    <q-menu anchor="top end" self="top start">
-                      <q-list dense style="min-width: 100px">
-                        <q-item clickable>
-                          <q-item-section @click="refresh">清空</q-item-section>
-                        </q-item>
-                      </q-list>
-                    </q-menu>
-                  </q-item>
-                </q-list>
-              </q-menu>
-            </div>
-            <div class="q-px-sm cursor-pointer non-selectable relative-position flex flex-center toolbar-a" v-ripple>
-              视图
-              <q-menu>
-                <q-list dense style="min-width: 100px;">
-                  <q-item clickable @click="shMinimap">
-                    <q-item-section>
-                      显示缩略图
-                    </q-item-section>
-                    <q-item-section side>
-                      <q-icon left name="check_box_outline_blank" class="q-mr-none" v-if="!showMinimap" />
-                      <q-icon left name="check_box" class="q-mr-none" v-else />
-                    </q-item-section>
-                  </q-item>
-                  <q-item clickable @click="shGrid">
-                    <q-item-section>
-                      显示网点
-                    </q-item-section>
-                    <q-item-section side>
-                      <q-icon left name="check_box_outline_blank" class="q-mr-none" v-if="!showGrid" />
-                      <q-icon left name="check_box" class="q-mr-none" v-else />
-                    </q-item-section>
-                  </q-item>
-                  <q-separator />
-                  <q-item clickable @click="zoomIn">
-                    <q-item-section>
-                      放大
-                    </q-item-section>
-                    <q-item-section side>
-                      Ctrl+滚轮向上
-                    </q-item-section>
-                  </q-item>
-                  <q-item clickable @click="zoomOut">
-                    <q-item-section>
-                      缩小
-                    </q-item-section>
-                    <q-item-section side>
-                      Ctrl+滚轮向下
-                    </q-item-section>
-                  </q-item>
-                  <q-separator />
-                  <q-item clickable @click="centerContent">
-                    <q-item-section>
-                      居中
-                    </q-item-section>
-                  </q-item>
-                  <q-item clickable @click="layout">
-                    <q-item-section>
-                      自动布局
-                    </q-item-section>
-                  </q-item>
-                  <q-separator />
-                  <q-item clickable @click="zoomTo(1)">
-                    <q-item-section>
-                      重置比例
-                    </q-item-section>
-                  </q-item>
-                  <q-item clickable @click="zoomToFit">
-                    <q-item-section>
-                      适应内容
-                    </q-item-section>
-                  </q-item>
-                </q-list>
-              </q-menu>
-            </div>
-            <div class="q-px-sm cursor-pointer non-selectable relative-position flex flex-center toolbar-a" v-ripple @click="help">
-              帮助
-            </div>
-          </div>
-        </q-toolbar-title>
-        <q-btn dense push color="primary" icon="autorenew" label="运行" @click="runWorkFlow" />
-      </q-toolbar>
-    </q-header>
+    <Header :flow="flow" :showGrid="showGrid" :showMinimap="showMinimap" />
     <LeftDrawer :showLeftDrawer="showLeftDrawer" />
     <RightDrawer ref="rightDrawer" :showRightDrawer="showRightDrawer" :flow="flow" />
     <div id="container">
@@ -206,6 +67,7 @@ import { Addon, Graph } from '@antv/x6';
 import { useQuasar, date } from 'quasar';
 import { defineComponent, onMounted, ref } from 'vue';
 import ToolBar from './ToolBar/ToolBar.vue';
+import Header from './Header/Header.vue';
 import LeftDrawer from './LeftDrawer/LeftDrawer.vue';
 import RightDrawer from './RightDrawer/RightDrawer.vue';
 const { Dnd } = Addon;
@@ -217,6 +79,7 @@ import ContextMenu from './Container/ContextMenu.vue';
 
 export default defineComponent({
   components: {
+    Header,
     ToolBar,
     LeftDrawer,
     RightDrawer,
