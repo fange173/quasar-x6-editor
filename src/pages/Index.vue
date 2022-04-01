a1bfcb8ee0a4f997b94d871a7753621029118226
Fange <928457421@qq.com>
2022-04-01 09:41:47 +0800

    format

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,15 +1,15 @@
 <template>
-<div class="full-width" style="height: calc(100vh - 50px)">
-  <X6Editor />
+  <div class="full-width" style="height: calc(100vh - 50px)">
+    <X6Editor />
   </div>
 </template>
 
 <script lang="ts">
-import X6Editor from "components/X6Editor/X6Editor.vue";
-import { defineComponent } from "vue";
+import X6Editor from 'components/X6Editor/X6Editor.vue';
+import { defineComponent } from 'vue';
 
 export default defineComponent({
-  name: "PageIndex",
+  name: 'PageIndex',
   components: { X6Editor },
   setup() {
     return {};
